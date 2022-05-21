107a870990f757e1310e5848adebeba2ce62307b
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-05-21 10:44:03 +0530

    Updated test as data driven approach

--- test/specs/smoke/login.spec.js
+++ test/specs/smoke/login.spec.js
@@ -1,10 +1,15 @@
 const LoginPage = require('../../page-objects/login.page');
+const fs = require('fs-extra');
+let userCreds = JSON.parse(fs.readFileSync('test/testdata/user-credentials.json'))
 
 describe('e2e test -login functionality', function () {
 
-    it('should be able to login', async function () {
-        await browser.url('https://www.saucedemo.com/');
-        await expect(browser).toHaveTitleContaining('Swag Labs');
-        await LoginPage.login('standard_user', 'secret_sauce!');
+    userCreds.forEach(({ username, password }) => {
+        it('should be able to login', async function () {
+            await browser.url('https://www.saucedemo.com/');
+            await expect(browser).toHaveTitleContaining('Swag Labs');
+            await LoginPage.login(username, password);
+        })
     })
+
 })
